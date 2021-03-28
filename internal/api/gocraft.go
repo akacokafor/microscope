c982dae930c9bf125acac53464a4914903510d66
Okafor Akachukwu <okafor.ak@outlook.com>
2021-03-28 12:53:30 +0100

    updated the busy_workers endpoint

--- internal/api/gocraft.go
+++ internal/api/gocraft.go
@@ -29,56 +29,7 @@ func NewGoCraftApi(options GoCraftOptions) *Context {
 		pool:      options.Pool,
 		client:    work.NewClient(options.Namespace, options.Pool),
 	}
-
-	// router.Middleware(func(c *Context, rw http.ResponseWriter, r *http.Request, next web.NextMiddlewareFunc) {
-	// 	c.Server = server
-	// 	next(rw, r)
-	// })
-	// router.Middleware(func(rw http.ResponseWriter, r *http.Request, next web.NextMiddlewareFunc) {
-	// 	rw.Header().Set("Content-Type", "application/json; charset=utf-8")
-	// 	next(rw, r)
-	// })
-	// router.Get("/queues", (*Context).queues)
-	// router.Get("/worker_pools", (*Context).workerPools)
-	// router.Get("/busy_workers", (*Context).busyWorkers)
-	// router.Get("/retry_jobs", (*Context).retryJobs)
-	// router.Get("/scheduled_jobs", (*Context).scheduledJobs)
-	// router.Get("/dead_jobs", (*Context).deadJobs)
-	// router.Post("/delete_dead_job/:died_at:\\d.*/:job_id", (*Context).deleteDeadJob)
-	// router.Post("/retry_dead_job/:died_at:\\d.*/:job_id", (*Context).retryDeadJob)
-	// router.Post("/delete_all_dead_jobs", (*Context).deleteAllDeadJobs)
-	// router.Post("/retry_all_dead_jobs", (*Context).retryAllDeadJobs)
-
-	// //
-	// // Build the HTML page:
-	// //
-	// assetRouter := router.Subrouter(Context{}, "")
-	// assetRouter.Get("/", func(c *Context, rw http.ResponseWriter, req *http.Request) {
-	// 	rw.Header().Set("Content-Type", "text/html; charset=utf-8")
-	// 	rw.Write(assets.MustAsset("index.html"))
-	// })
-	// assetRouter.Get("/work.js", func(c *Context, rw http.ResponseWriter, req *http.Request) {
-	// 	rw.Header().Set("Content-Type", "application/javascript; charset=utf-8")
-	// 	rw.Write(assets.MustAsset("work.js"))
-	// })
-
-	// return server
 }
-
-// Start starts the server listening for requests on the hostPort specified in NewServer.
-// func (w *Server) Start() {
-// 	w.wg.Add(1)
-// 	go func(w *Server) {
-// 		w.server.ListenAndServe()
-// 		w.wg.Done()
-// 	}(w)
-// }
-
-// // Stop stops the server and blocks until it has finished.
-// func (w *Server) Stop() {
-// 	w.server.Close()
-// 	w.wg.Wait()
-// }
 
 func (c *Context) queues(rw http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	response, err := c.client.Queues()
@@ -115,6 +66,7 @@ func (c *Context) busyWorkers(rw http.ResponseWriter, r *http.Request, _ httprou
 			"entries": busyObservations,
 		}
 		render(rw, data, err)
+		return
 	}
 
 	render(rw, map[string]interface{}{
